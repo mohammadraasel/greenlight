1db222f05c10c2cd64df180a767fc7cfa93f1670
mohammadraasel <mohammadrasel1221@gmail.com>
2021-09-12 06:43:05 +0600

    setup http server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	logger *log.Logger
+}
+
+func (a *application) healthcheckHandler(res http.ResponseWriter, req *http.Request) {
+
+}
 
 func main() {
-	fmt.Println("Hey, Mr. X")
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 5000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	logger.Printf("Starting %s server on %s", cfg.env, srv.Addr)
+	err := srv.ListenAndServe()
+	logger.Fatal(err)
 }
