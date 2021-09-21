b189491467d470ef0c93cbe8b333133027e55f40
mohammadraasel <mohammadrasel1221@gmail.com>
2021-09-22 00:21:00 +0600

    refactor main.go file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"context"
-	"database/sql"
 	"errors"
 	"flag"
-	"fmt"
-	"log"
-	"net/http"
 	"os"
-	"time"
 
 	dotENV "github.com/joho/godotenv"
 	_ "github.com/lib/pq"
@@ -75,64 +69,10 @@ func main() {
 		models: data.NewModels(db),
 	}
 
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      app.routes(),
-		ErrorLog:     log.New(logger, "", 0),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
-	}
-
-	logger.PrintInfo("Starting server", map[string]string{
-		"addr": srv.Addr,
-		"env":  cfg.env,
-	})
-	err = srv.ListenAndServe()
-	logger.PrintFatal(err, nil)
-}
-
-func openDB(cfg config) (*sql.DB, error) {
-	// Use sql.Open() to create an empty connection pool, using the DSN from the config
-	// struct.
-	db, err := sql.Open("postgres", cfg.db.dsn)
+	err = app.serve()
 	if err != nil {
-		return nil, err
+		logger.PrintFatal(err, nil)
 	}
-
-	// Set the maximum number of open (in-use + idle) connections in the pool. Note that
-	// passing a value less than or equal to 0 will mean there is no limit.
-	db.SetMaxOpenConns(cfg.db.maxOpenConns)
-
-	// Set the maximum number of idle connections in the pool. Again, passing a value
-	// less than or equal to 0 will mean there is no limit.
-	db.SetMaxIdleConns(cfg.db.maxIdleConns)
-
-	// Use the time.ParseDuration() function to convert the idle timeout duration string
-	// to a time.Duration type.
-	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
-	if err != nil {
-		return nil, err
-	}
-
-	// Set the maximum idle timeout.
-	db.SetConnMaxIdleTime(duration)
-
-	// Create a context with a 5-second timeout deadline.
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	// Use PingContext() to establish a new connection to the database, passing in the
-	// context we created above as a parameter. If the connection couldn't be
-	// established successfully within the 5 second deadline, then this will return an
-	// error.
-	err = db.PingContext(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	// Return the sql.DB connection pool.
-	return db, nil
 }
 
 func loadEnvs(logger *jsonlog.Logger) {
