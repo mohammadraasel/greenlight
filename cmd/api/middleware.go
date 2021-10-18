bc950a7fea5cc0a6b3fa6e6da5c5eee33425f3c2
mohammadraasel <mohammadrasel1221@gmail.com>
2021-10-19 01:55:04 +0600

    Enable  cors

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -225,3 +225,11 @@ func (app *application) requirePermission(code string, next httprouter.Handle) h
 	// Wrap this with the requireActivatedUser() middleware before returning it.
 	return app.requireActivatedUser(fn)
 }
+
+func (app *application) enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		next.ServeHTTP(w, r)
+	})
+}
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -24,5 +24,5 @@ func (app *application) routes() http.Handler {
 	router.PUT("/v1/users/activated", app.activateUser)
 	router.POST("/v1/tokens/authentication", app.createAuthenticationToken)
 
-	return app.recoverPanic(app.rateLimit(app.authenticate(router)))
+	return app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router))))
 }
